8742559b8d75a7dc1c5354bc5bc0bc66f053fcc1
moizest89 <moises.gilberto@gmail.com>
2017-08-14 17:02:30 -0400

    my first commit

---
+++ app/src/main/java/moizest89/reigndesignevaluation/ui/main/MainActivityFragment.java
@@ -0,0 +1,24 @@
+package moizest89.reigndesignevaluation.ui.main;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import moizest89.reigndesignevaluation.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
