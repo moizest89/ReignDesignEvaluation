8dfe14dc71b188a02991bfbaa63c4975fafdea4d
moizest89 <moises.gilberto@gmail.com>
2017-08-16 00:03:01 -0400

    view

--- app/src/main/java/moizest89/reigndesignevaluation/ui/article/details/ArticleDetailsActivity.java
+++ app/src/main/java/moizest89/reigndesignevaluation/ui/article/details/ArticleDetailsActivity.java
@@ -1,23 +1,45 @@
 package moizest89.reigndesignevaluation.ui.article.details;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import moizest89.reigndesignevaluation.R;
+import moizest89.reigndesignevaluation.data.models.Hit;
 
 public class ArticleDetailsActivity extends AppCompatActivity {
-    
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.web_view)
+    WebView web_view;
+    @BindView(R.id.progress_bar)
+    ProgressBar progress_bar;
+
+
+    private Hit hit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_details);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+
+
+        Intent intent = getIntent();
+        if(intent != null){
+            
+        }
 
     }
 
