031476f3fbf587618f45b22fbd822c051a9cccf9
moizest89 <moises.gilberto@gmail.com>
2017-08-21 12:35:33 -0500

    set custom font and change ic_launcher

--- app/src/main/java/moizest89/reigndesignevaluation/ui/main/MainApplication.java
+++ app/src/main/java/moizest89/reigndesignevaluation/ui/main/MainApplication.java
@@ -3,6 +3,8 @@ package moizest89.reigndesignevaluation.ui.main;
 import android.app.Application;
 
 import io.realm.Realm;
+import moizest89.reigndesignevaluation.R;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 /**
  * Created by moizest89 on 8/16/17.
@@ -17,5 +19,11 @@ public class MainApplication extends Application{
 
         Realm.init(this);
 
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/roboto_condensed.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
     }
 }
